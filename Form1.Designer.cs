87c362bd41c278762f65ec4ac48d2b1cce2c6993
moneyflow <moneyflowbeatz@gmail.com>
2024-05-07 20:26:54 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(222, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Смена имени аудиофайла";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Каталог:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Выбрать каталог";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(507, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(539, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите имя для \"Название\":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите имя для \"Исполнитель\":";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(507, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 313);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AudioTagsChanger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TagLib;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolder = folderBrowser.SelectedPath;
+                textBox1.Text = selectedFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string selectedFolder = textBox1.Text; 
+            string newDescription = textBox2.Text;
+            string newPerformer = textBox3.Text;
+            string AlbumName = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(selectedFolder) && !string.IsNullOrEmpty(newDescription))
+            {
+                string[] audioFiles = Directory.GetFiles(selectedFolder, "*.*", SearchOption.AllDirectories); 
+                List<string> audioExtensions = new List<string> { ".mp3", ".wav", ".flac", ".aac", ".ogg", ".m4a" }; 
+
+                foreach (string file in audioFiles)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (audioExtensions.Contains(extension))
+                    {
+                        try
+                        {
+                            TagLib.File audioFile = TagLib.File.Create(file);
+                            audioFile.Tag.Title = newDescription;
+                            audioFile.Tag.Comment = newDescription;
+                            audioFile.Tag.Conductor = newDescription;
+                            audioFile.Tag.Copyright = newDescription;
+                            audioFile.Tag.Genres = new string[] { newDescription };
+                            audioFile.Tag.Lyrics = newDescription;
+                            audioFile.Tag.Publisher = newDescription;
+                            audioFile.Tag.RemixedBy = newDescription;
+                            audioFile.Tag.Subtitle = newDescription;
+                            audioFile.Tag.Year = 2024;
+                            audioFile.Tag.Performers = new string[] { newPerformer }; 
+                            audioFile.Tag.Composers = new string[] { AlbumName };
+                            audioFile.Tag.AlbumArtists = new string[] { newPerformer };
+                            audioFile.Save();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Ошибка при переименовании описания: " + file + "\n" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+
+                MessageBox.Show("Описание аудиофайлов изменено.", "Успешно"); 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(textBox1, "Тут появится путь к вашей папке после нажатия на кнопку ниже.");
+            toolTip1.SetToolTip(button1, "Выборите каталог в появившемся меню после нажатия на кнопку");
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(toolTip1.GetToolTip(textBox1), textBox1, textBox1.Width, 0);
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(textBox1);
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(toolTip1.GetToolTip(button1), button1, button1.Width, 0);
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(button1);
+        }
+    }
+}
